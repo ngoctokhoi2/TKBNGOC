0ee240474d394c2951c854a0cd411263a25ff973
ngoctokhoi2 <ngoctokhoi2@gmail.com>
2026-09-18 18:32:33 +0700

    chore: configure base path for GitHub Actions deployment
    Update the base URL to dynamic to support local development and GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    Base:'/LBGNGOC/'.
+    base: process.env.GITHUB_ACTIONS ? '/LBGNGOC/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
